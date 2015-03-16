359799167fea7e9aebb55a0d51de8a72a44dfb84
eric neunaber <eneunaber@gmail.com>
2015-03-16 07:06:25 -0700

    adding/updating more files used to fix player data

--- sql/generate-sql-for-missing-idconversion.sql
+++ sql/generate-sql-for-missing-idconversion.sql
@@ -12,6 +12,73 @@ WHERE ypms.scrubbedname NOT IN (
 ) and  pl.MLBCODE is not null and  pl.MLBCODE <> ''
 
 
+
+SELECT 'Insert into dbo.IdConversion (mlbamID, Name, BDB, statsID, cbsID, espnFantasyID) values  ('''+ pl.MLBCODE + ''', ''' + ypms.scrubbedName + ''', '''', ''' + ypms.playerid + ''', '''', '''' )'
+FROM [fantasy.baseball].[dbo].YahooPlayerMappingsScrubbed ypms
+join [fantasy.baseball].[dbo].playerid_list pl
+	on ypms.scrubbedname = (pl.FIRSTNAME + ' ' + pl.LASTNAME)
+WHERE ypms.scrubbedname NOT IN (
+		SELECT idc.Name FROM [fantasy.baseball].[dbo].[IdConversion] idc
+) and  pl.MLBCODE is not null and  pl.MLBCODE <> ''
+and ypms.playerid in
+(
+9002,
+9157,
+9850,
+7283,
+6314,
+9094,
+9096,
+8207,
+6708,
+9298,
+7572,
+8333,
+7837,
+9220,
+9270,
+9443,
+9350,
+9837,
+8061,
+7571,
+9266,
+9410,
+9787,
+9163,
+9638,
+9543,
+9555,
+9824,
+7333,
+9498,
+9462,
+9678,
+9641,
+6109,
+9673,
+9583,
+9210,
+9139,
+9338,
+9009,
+8373,
+9754,
+9307,
+9509,
+9718,
+9784,
+7814,
+9766,
+9778,
+9884,
+9842,
+9539
+)
+
+
+
+
  update st
  set mlbamid = idc.mlbamID
  from dbo.[2015BattingSteamer] as st
---
+++ sql/temp.sql
@@ -0,0 +1,50 @@
+J.D. Martinez	502110 9002
+A.J. Pollock	572041 9157
+Yasmany Tomas	630111 9850
+J.J. Hardy	429666 7283
+A.J. Burnett	150359 6314
+Norichika Aoki	493114 9094
+Darren O'Day	503285 8207
+R.A. Dickey	285079 6708
+A.J. Ramos	573109 9298
+J.P. Howell	434442 7572
+Jon Niese	477003 8333
+A.J. Griffin	456167 9220
+Tom Layne	518927 9270
+T.J. House	543334 9443
+T.J. McFarland	519008 9350
+R.J. Alvarez	571439 9837
+J.A. Happ	457918 8061
+C.J. Wilson	450351 7571
+Steven Geltz	544993 9266
+Nate Karns	501992 9410
+J.J. Hoover	543331 9163
+A.J. Cole	595918 9638
+J.R. Graham	543242 9543
+C.J. Edwards	605218  9555
+A.J. Achter	592091 9824
+C.C. Lee	547348 9462
+C.J. Riefenhauser	592677 9678
+A.J. Pierzynski	150229 6109
+Michael Bolsinger	502211 9673
+C.J. Cron	543068 9583
+Kris Negron	502117 9210
+Jackie Bradley	598265 9338
+J.B. Shuck	543776 9009
+A.J. Ellis	454560 8373
+L.J. Hoes	543321 9307
+J.T. Realmuto	592663 9718
+Tony Gwynn	448242 7814
+
+
+
+Travis dArnaud	9096
+Eric OFlaherty	7837
+Alexander Claudio	592222
+Melvin Upton Jr	7333
+Matt West	9498
+Miguel Alfredo Gonzalez	9641
+Robbie Ross Jr	9139
+Kike Hernandez	9754 
+John Ryan Murphy	9509
+Michael A Taylor	9784
